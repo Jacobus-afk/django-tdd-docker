0c7086892bcfe6bd4e626b4f2876d3497f39e638
Cobus Linde <barendl@staycontactable.com>
2022-08-21 14:48:03 +0200

    APIs updated to use DRF router

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -1,22 +1,31 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import MovieList, MovieDetail, MovieViewSet
+from .views import MovieViewSet
 
-movie_list = MovieViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
+from rest_framework.routers import DefaultRouter
 
-movie_detail = MovieViewSet.as_view({
-    'get': 'retrieve'
-})
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet, basename='movies')
+
+# movie_list = MovieViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+
+# movie_detail = MovieViewSet.as_view({
+#     'get': 'retrieve'
+# })
 
 # urlpatterns = [
 #     path("api/movies/", MovieList.as_view()),
 #     path("api/movies/<int:pk>/", MovieDetail.as_view()),
 # ]
 
+# urlpatterns = [
+#     path("api/movies/", movie_list, name='movie-list'),
+#     path("api/movies/<int:pk>/", movie_detail, name='movie-detail'),
+# ]
+
 urlpatterns = [
-    path("api/movies/", movie_list, name='movie-list'),
-    path("api/movies/<int:pk>/", movie_detail, name='movie-detail'),
+    path('', include(router.urls)),
 ]
